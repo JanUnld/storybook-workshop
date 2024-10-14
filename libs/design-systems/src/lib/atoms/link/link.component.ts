46baec13bdbd491941583aeb17e205f23ce155a7
Jan Unnold <jan.unnold@novatec-gmbh.de>
2024-10-14 16:57:40 +0200

    Setup link atom

---
+++ libs/design-systems/src/lib/atoms/link/link.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ds-link',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './link.component.html',
+  styleUrl: './link.component.css',
+})
+export class LinkComponent {}
