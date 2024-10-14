6a7868b02c1e81b80d0293301689a5ecd1456190
Jan Unnold <jan.unnold@novatec-gmbh.de>
2024-10-14 17:35:57 +0200

    Implemented link atom + stories

--- libs/design-systems/src/lib/atoms/link/link.component.stories.ts
+++ libs/design-systems/src/lib/atoms/link/link.component.stories.ts
@@ -1,4 +1,11 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { importProvidersFrom } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {
+  applicationConfig,
+  Meta,
+  moduleMetadata,
+  StoryObj,
+} from '@storybook/angular';
 import { LinkComponent } from './link.component';
 
 import { within } from '@storybook/testing-library';
@@ -6,19 +13,36 @@ import { expect } from '@storybook/jest';
 
 const meta: Meta<LinkComponent> = {
   component: LinkComponent,
-  title: 'LinkComponent',
+  title: 'Atoms/LinkComponent',
 };
 export default meta;
-type Story = StoryObj<LinkComponent>;
 
-export const Primary: Story = {
-  args: {},
+type Story = StoryObj<LinkComponent & { text: string }>;
+
+const render: Story['render'] = (args) => ({
+  template: `<ds-link [href]="'${args.href}'" [openInNewTab]="${args.openInNewTab}">${args.text}</ds-link>`,
+});
+
+const play: Story['play'] = async ({ canvasElement, args }) => {
+  const canvas = within(canvasElement);
+  expect(canvas.getByText(args.text)).toBeTruthy();
 };
 
-export const Heading: Story = {
-  args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/link works!/gi)).toBeTruthy();
+export const Default: Story = {
+  render,
+  args: {
+    text: 'Link works!',
+    href: 'https://storybook.js.org',
+    openInNewTab: false,
   },
+  play,
+};
+
+export const OpenInNewTab: Story = {
+  render,
+  args: {
+    ...Default.args,
+    openInNewTab: true,
+  },
+  play,
 };
--- libs/design-systems/src/lib/atoms/link/link.component.ts
+++ libs/design-systems/src/lib/atoms/link/link.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'ds-link',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './link.component.html',
   styleUrl: './link.component.css',
+  template: `
+    <a [href]="href()" [target]="target()" referrerpolicy="no-referrer">
+      <ng-content></ng-content>
+    </a>
+  `,
 })
-export class LinkComponent {}
+export class LinkComponent {
+  /** Hyper reference link to be set for the underlying {@link HTMLAnchorElement} */
+  href = input<string>();
+  /** Defines whether the link shall be opened in a new window tab or not */
+  openInNewTab = input(false);
+
+  target = computed(() => (this.openInNewTab() ? '_blank' : '_self'));
+}
