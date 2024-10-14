46f3dd23f9468960c30150ac6f053c576acb476d
Jan Unnold <jan.unnold@novatec-gmbh.de>
2024-10-14 16:29:37 +0200

    Angular `design-system` lib

---
+++ libs/design-systems/src/lib/design-system/design-system.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ds-design-system',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './design-system.component.html',
+  styleUrl: './design-system.component.css',
+})
+export class DesignSystemComponent {}
